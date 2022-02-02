a1a5a49dc1ce4c4ac58d7072e2b37f6dd93e60ee
Fernando Crelick <fccrelick@protonmail.com>
2022-02-02 13:24:24 -0300

    created the Main Coordinator

---
+++ CoordinatorPattern/MainCoordinator.swift
@@ -0,0 +1,22 @@
+//
+//  MainCoordinator.swift
+//  CoordinatorPattern
+//
+//  Created by Fernando Crelick on 02/02/22.
+//
+
+import UIKit
+
+class MainCoordinator: Coordinator {
+    var navigationController: UINavigationController?
+
+    func eventOccurred(with type: Event) {
+
+    }
+
+    func start() {
+        
+    }
+
+
+}
